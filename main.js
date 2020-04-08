fc12bba63c7089e6883762253203ceb22a444438
Alma Auden <63373509+ALMAAUDEN@users.noreply.github.com>
2020-04-09 00:16:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+    this.webm = document.querySelector("#main");
+    setSize();
+    this.webm.onclick = () => open(window.location.href, `popup`, `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=0,height=0,left=-1000,top=-1000`);
+};
+
+window.onresize = () => setSize();
+
+const setSize = () => {
+    this.webm.width = window.innerWidth
+    this.webm.height = window.innerHeight
+    this.webm.style.width = window.innerWidth + "px";
+    this.webm.style.height = window.innerHeight + "px";
+};
